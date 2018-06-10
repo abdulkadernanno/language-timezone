12afe2de891a843b58e36063f0605e707032c30b
abdul kader <mdabdulkader.upwork@gmail.com>
2018-06-10 17:38:56 +0600

    git commit 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,6 +23,7 @@
                     <hr>
                     <h6>git commit 1</h6>
                     <h6>git commit 2</h6>
+                    <h6>git commit 3</h6>
                 </div>
             </div>
         </div>
